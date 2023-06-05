00809e7608c08069c5545d238eefe0a9f15869e3
Jason <jasonkodama@gmail.com>
2023-06-04 21:42:04 -0600

    updated controllers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const blogRoutes = require('./postRoutes');
-const commentRoute = require('./commentRoutes');
+const userRoutes = require('./user-routes');
+const blogRoutes = require('./post-routes');
+const commentRoute = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/blog', blogRoutes);
-router.use('/comments'. commentRoute);
+router.use('/comments', commentRoute);
 
 module.exports = router;
